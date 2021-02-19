eebf9a32e2b2b33ce953f5c07ea66fa5d925e5b6
alvynah <alvynahw@gmail.com>
2021-02-19 16:35:00 +0300

    Add the switch case control flow for the display results function

--- js/index.js
+++ js/index.js
@@ -2,12 +2,15 @@ var maleName = ["kwasi", "Kwado", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
 var femaleName = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
 var days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
+
+
 function getName(event) {
-    event.preventDefault;
+    event.preventDefault();
     let myName = document.getElementById("name").value;
+    return myName;
 
 
-};
+}
 
 function getDay(event) {
     event.preventDefault();
@@ -17,12 +20,19 @@ function getDay(event) {
     var compare = dayString.toString(); //convert it to string
 
     var day = days.indexOf(compare); //find index ot string day in the array days
+    return day;
 
-};
+}
 
 
-function getGender() {
-    var genders = document.getElementsByName("gender").value;
+function getGender(event) {
+    event.preventDefault();
+    var returnGetname = getName(event);
+    var returnGetDay = getDay(event);
+    var message;
+
+    getDay(event);
+    var genders = document.getElementsByName("gender");
     if (genders[0].checked == true) {
         var gender = "female";
 
@@ -33,51 +43,54 @@ function getGender() {
     }
     switch (gender) {
         case "male":
-            if (day == 0) {
-                let message = ``
-                alert("You were born on " + dayNames[0] + " and Your akan name is " + maleNames[0] + "!");
-            } else if (dayValue == 2) {
-                alert("You were born on " + dayNames[1] + " and Your akan name is " + maleNames[1] + "!");
-            } else if (dayValue == 3) {
-                alert("You were born on " + dayNames[2] + " and Your akan name is " + maleNames[2] + "!");
-            } else if (dayValue == 4) {
-                alert("You were born on " + dayNames[3] + " and Your akan name is " + maleNames[3] + "!");
-            } else if (dayValue == 5) {
-                alert("You were born on " + dayNames[4] + " and Your akan name is " + maleNames[4] + "!");
-            } else if (dayValue == 6) {
-                alert("You were born on " + dayNames[5] + " and Your akan name is " + maleNames[5] + "!");
-            } else if (dayValue == -0) {
-                alert("You were born on " + dayNames[6] + " and Your akan name is " + maleNames[6] + "!");
+            if (returnGetDay == 0) {
+                message = `${returnGetname}, your were born on ${days[0]}, and your Akan Name is ${maleName[0]} !`
+            } else if (returnGetDay == 1) {
+                message = `${returnGetname}, your were born on ${days[1]}, and your Akan Name is ${maleName[1]} !`
+            } else if (returnGetDay == 2) {
+                message = `${returnGetname}, your were born on ${days[2]}, and your Akan Name is ${maleName[2]} !`
+            } else if (returnGetDay == 3) {
+                message = `${returnGetname}, your were born on ${days[3]}, and your Akan Name is ${maleName[3]} !`
+            } else if (returnGetDay == 4) {
+                message = `${returnGetname}, your were born on ${days[4]}, and your Akan Name is ${maleName[4]} !`
+            } else if (returnGetDay == 5) {
+                message = `${returnGetname}, your were born on ${days[5]}, and your Akan Name is ${maleName[5]} !`
+            } else if (returnGetDay == 6) {
+                message = `${returnGetname}, your were born on ${days[6]}, and your Akan Name is ${maleName[6]} !`
+            } else {
+                return false;
             }
             break;
-        case "female":
-            if (dayValue == 1) {
-                alert("You were born on " + dayNames[0] + " and Your akan name is  " + femaleNames[0] + "!");
-            } else if (dayValue == 2) {
-                alert("You were born on " + dayNames[1] + " and Your akan name is " + femaleNames[1] + "!");
-            } else if (dayValue == 3) {
-                alert("You were born on " + dayNames[2] + " and Your akan name is " + femaleNames[2] + "!");
-            } else if (dayValue == 4) {
-                alert("You were born on " + dayNames[3] + " and Your akan name is " + femaleNames[3] + "!");
-            } else if (dayValue == 5) {
-                alert("You were born on " + dayNames[4] + " and Your akan name is " + femaleNames[4] + "!");
-            } else if (dayValue == 6) {
-                alert("You were born on " + dayNames[5] + " and Your akan name is " + femaleNames[5] + "!");
-            } else if (dayValue == -0) {
-                alert("You were born on " + dayNames[6] + " and Your akan name is " + femaleNames[6] + "!");
+        case "famale":
+            if (returnGetDay == 0) {
+                message = `${returnGetname}, your were born on ${days[0]}, and your Akan Name is ${femaleName[0]} !`
+            } else if (returnGetDay == 1) {
+                message = `${returnGetname}, your were born on ${days[1]}, and your Akan Name is ${femaleName[1]} !`
+            } else if (returnGetDay == 2) {
+                message = `${returnGetname}, your were born on ${days[2]}, and your Akan Name is ${femaleName[2]} !`
+            } else if (returnGetDay == 3) {
+                message = `${returnGetname}, your were born on ${days[3]}, and your Akan Name is ${femaleName[3]} !`
+            } else if (returnGetDay == 4) {
+                message = `${returnGetname}, your were born on ${days[4]}, and your Akan Name is ${femaleName[4]} !`
+            } else if (returnGetDay == 5) {
+                message = `${returnGetname}, your were born on ${days[5]}, and your Akan Name is ${femaleName[5]} !`
+            } else if (returnGetDay == 6) {
+                message = `${returnGetname}, your were born on ${days[6]}, and your Akan Name is ${femaleName[6]} !`
+            } else {
+                return false;
             }
             break
         default:
 
     }
+    return message;
 }
 
 function getResults(event) {
 
     event.preventDefault();
-
+    var results = getGender(event);
     let displayTag = document.getElementById("displayarea");
-    let results = "Love";
     displayTag.innerHTML = results;
 
-}
+};
